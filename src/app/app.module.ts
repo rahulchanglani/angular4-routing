8fa6574c071c54b0c68642449871643c1b90e302
Rahul Changlani <rahul.changlani@gmail.com>
2017-08-07 14:07:06 +0530

    navigate prohgrammatically

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,10 +14,14 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 
+
 const appRoutes: Routes = [
   { path: '',  component: HomeComponent },
   { path: 'users',  component: UsersComponent },
-  { path: 'servers',  component: ServersComponent }
+  { path: 'servers',  component: ServersComponent },
+  { path: 'servers',  component: ServersComponent },
+  { path: 'servers',  component: ServersComponent },
+  { path: 'servers',  component: ServersComponent },
 ];
 
 @NgModule({

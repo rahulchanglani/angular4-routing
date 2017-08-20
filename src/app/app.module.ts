69779cf14d1339a92278d91d534eed73b5874398
Rahul Changlani <rahul.changlani@gmail.com>
2017-08-20 23:37:55 +0530

    redirecting & wildcard routes, matching, outsourcing route config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { Routes, RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
@@ -13,19 +13,7 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-
-
-const appRoutes: Routes = [
-  { path: '',  component: HomeComponent },
-  { path: 'users',  component: UsersComponent, children: [
-    { path: ':id/:name',  component: UserComponent }    
-  ] },
-  { path: 'servers',  component: ServersComponent, children: [
-    { path: ':id',  component: ServerComponent },
-    { path: ':id/edit',  component: EditServerComponent }
-  ] },
-  
-];
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
@@ -35,13 +23,14 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
